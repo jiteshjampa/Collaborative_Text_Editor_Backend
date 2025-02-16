1820705c3e8ec5b8b8f73f118bce5b83f4aecbb7
jiteshjampa <jampajitesh@gmail.com>
2025-02-16 17:23:03 +0530

    updated index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const io = new Server(server, { cors: { origin: "*" } });
 
 app.use(cors());
 app.use(express.json());
-
+app.get("/", (req, res) => res.send("API running"));
 connectDB();
 app.use("/api/docs", documentRoutes);
 
